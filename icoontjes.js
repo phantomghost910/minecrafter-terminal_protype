7b3c46c06b0544b9b663ce2c3d6f76e926eda850
NicolasSzczepanek103440 <103440@glr.nl>
2026-05-23 16:13:40 +0200

    23/05/2026:
    prototype start-up scherm gemaakt

---
+++ icoontjes.js
@@ -0,0 +1,63 @@
+let icoontjes_container = document.querySelector(".icoontjes_container");
+
+
+// Hoofscherm array \/ \/
+let hoofdscherm_appsfolders = {
+    "My Computer": "app",
+    "Network Neighborhood": "app",
+    "Inbox": "app",
+    "Recycle Bin": "app",
+    "My Briefcase": "folder"
+}
+/* ^^ Hoofdscherm apps/folders en icoontjes_container horen samen te werken. */
+
+/* Functie om apps en folders toe te voegen \/ \/ \/
+    (naam is voor de naam van de bestand,
+    image is het url van de icoontje)*/ 
+function addFolderApp(naam, image) {
+        let icoontje_template = document.createElement("div");
+        let icoontje = document.createElement("div");
+        let icoontje_afbeelding = document.createElement("img");
+        let icoontje_text = document.createElement("div");
+
+        icoontje_template.className += "icoontje_template";
+        icoontje.className += "icoontje";
+        icoontje_text.className += "icoontje_text";
+        icoontje_afbeelding.src = image;
+
+        icoontjes_container.appendChild(icoontje_template);
+        icoontje_template.appendChild(icoontje);
+        icoontje.appendChild(icoontje_afbeelding);
+        icoontje_template.appendChild(icoontje_text);
+
+        icoontje_text.innerHTML = naam;
+}
+
+
+// Hier worden alle namen in de hoofdscherm array gecheckt en toegevoegd volgens de naam. \/ \/ \/
+for (let key in hoofdscherm_appsfolders) {
+    if (key == "My Computer") {
+        console.log("my computer files goed ingesteld (verander dit later in js)");
+        addFolderApp("My Computer", "images/app_template.png");
+    }
+
+    if (key == "Network Neighborhood") {
+        console.log("network neighborhood files goed ingesteld (verander dit later in js)");
+        addFolderApp("Network Neighborhood", "images/app_template.png");
+    }
+
+    if (key == "Inbox") {
+        console.log("Inbox files goed ingesteld (verander dit later in js)");
+        addFolderApp("Inbox", "images/app_template.png");
+    }
+
+    if (key == "Recycle Bin") {
+        console.log("Recycle bin files goed ingesteld (verander dit later in js)");
+        addFolderApp("Recycle Bin", "images/app_template.png");
+    }
+
+    if (key == "My Briefcase") {
+        console.log("My briefcase files goed ingesteld (verander dit later in js)");
+        addFolderApp("My Briefcase", "images/folder_template.png");
+    }
+}
---
+++ start_up.js
@@ -0,0 +1,28 @@
+let startup_scherm = document.getElementById("start_up");
+let startupscherm_logo = document.getElementById("c_and_a_logo_startup");
+
+startupscherm_logo.style.display = "none";
+startup_scherm.style.background = "black";
+
+function startup() {
+    startup_scherm.setAttribute("onclick", "");
+    setTimeout(function() {
+    startupscherm_logo.style.display = "flex";
+    startup_scherm.style.background = "url(images/background_startup.png)";
+    startup_scherm.style.backgroundSize = "cover";
+    startup_scherm.style.backgroundPosition = "center";
+
+    let startup_sound = document.createElement("audio");
+        let startupsound_source = document.createElement("source");
+
+        startupscherm_logo.appendChild(startup_sound);
+        startup_sound.appendChild(startupsound_source);
+        startup_sound.autoplay = true;
+        startupsound_source.src = "sounds/placeholder_startupnoise.mp3";
+        startupsound_source.type = "audio/mp3";
+    }, 3000)
+
+    setTimeout(function() {
+        startup_scherm.style.display = "none";
+    }, 11000)
+}
