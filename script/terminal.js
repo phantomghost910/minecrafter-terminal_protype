a9e81b412a3225cfed7ff68632006d3cfca06ece
phantomghost910 <103609@glr.nl>
2026-06-10 08:45:03 +0200

    terminal pop up console log

--- script/terminal.js
+++ script/terminal.js
@@ -1,6 +1,5 @@
 
-function terminal(){
+function popupOpen(){
     console.log('click op terminal');
-    let open = document.getElementById("terminal_container");
-    open.classList.add("terminal_open");
+
 }
