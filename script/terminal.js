03d005f6e197684f2ef6017eb20272ee20bbb98f
phantomghost910 <103609@glr.nl>
2026-06-16 12:28:09 +0200

    infrastructure terminal added

--- script/terminal.js
+++ script/terminal.js
@@ -14,31 +14,37 @@ function closing(){
 
 // beging binnen terminal
 
-let input = document.getElementById('input_station');
+var input = document.getElementById('input_station');
+var write = document.getElementById("")
+var check = document.getElementById("if_statement_check")
+
 
 input.addEventListener('keypress', function(event) {
     
     let command = input.value;
     if (event.key === 'Enter') {
         event.preventDefault();
-        console.log('Command entered' + command);
             if (command === "rickroll" ){
             window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
             console.log('rickroll');
         }
-            if (command === i){
-
+            if (command === "systemctl root mount 'open'/kinger/ delete"){
+                check.setAttribute('class', 'check_1')
         }
-            if (command === i){
-            
+            if (command === "" && check.contains("check_1")){
+                check.removeAttribute('class', 'check_1')
+                check.setAttribute('class', 'check_2')
         }
-            if (command === i){
-            
+            if (command === "" && check.contains("check_2")){
+                check.removeAttribute('class', 'check_2')
+                check.setAttribute('class', 'check_3')
         }
-            if (command === i){
-            
+            if (command === "" && check.contains("check_3")){
+                check.removeAttribute('class')
         }
-
+        else {
+            console.log("warning not a command")
+        }
 
     }
 
