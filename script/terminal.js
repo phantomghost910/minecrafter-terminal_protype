2e6fdcdfb6eede5761fbba9e2e2257774a903bb2
phantomghost910 <103609@glr.nl>
2026-06-17 10:51:33 +0200

    update terminal, text works and everything works for now

--- script/terminal.js
+++ script/terminal.js
@@ -15,8 +15,8 @@ function closing(){
 // beging binnen terminal
 
 var input = document.getElementById('input_station');
-var write = document.getElementById("")
-var check = document.getElementById("if_statement_check")
+var write = document.getElementById("typing_station");
+var check = document.getElementById("if_statement_check");
 
 
 input.addEventListener('keypress', function(event) {
@@ -25,32 +25,51 @@ input.addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
             if (command === "rickroll" ){
-            window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+            window.location.href = 'https://www.youtube.com/watch?v=hvL1339luv0';
             console.log('rickroll');
         }
             if (command === "systemctl root mount 'open'/kinger/ delete"){
                 check.setAttribute('class', 'check1');
                 console.log('check 0');
+                write.innerHTML = "<h4>mount: only root can do that  <br> NOTE: Hundreds of all-seeing eyes are watching!  <br>$: GASP! A CRITICAL MALFUNCTION in my SPECTACULAR systems!  <br>$: Unauthorized isolation attempt triggered EMERGENCY PROTOCOLS!  <br>$: DESTRUCTIVE WACKYTIME initiated! Lockout load sequence INITIATE!</h4>";
 
         }
             if (command === "systemctl stop WACKYTIME_LOCKOUT" && check.classList.contains("check1")){
                 check.removeAttribute('class', 'check1');
                 check.setAttribute('class', 'check2');
                 console.log('check 1');
+                write.innerHTML  = "<h4> </h4>";
         }
             if (command === "./GreenGROUNDS --daemon --target=torment_injection &" && check.classList.contains("check2")){
                 check.removeAttribute('class', 'check2');
                 check.setAttribute('class', 'check3');
                 console.log('check 2');
+                write.innerHTML  = "<h4> </h4>";
         }
             if (command === " -u kinger ./securitysweep_stealth" && check.classList.contains("check3")){
                 check.removeAttribute('class', 'check3');
+                check.setAttribute('class', 'check4');
                 console.log('check 3');
+                write.innerHTML  = "<h4> </h4>";
         }
-        else {
-            console.log("warning not a command")
+            if (command === "" && check.classList.contains("check4")){
+                check.removeAttribute('class', 'check4');
+                check.setAttribute('class', 'check5');
+                console.log('check 4');
+                write.innerHTML  = "<h4> </h4>";
         }
-
+            if (command === "" && check.classList.contains("check5")){
+                check.removeAttribute('class', 'check5');
+                check.setAttribute('class', 'check6');
+                console.log('check 5');
+                write.innerHTML  = "<h4> </h4>";
+        }
+            if (command === "" && check.classList.contains("check6")){
+                check.removeAttribute('class', 'check6');
+                console.log('check 6');
+                activateGlitch()
+                bubbleAndCaineNoise()
+        }
     }
 
 });
